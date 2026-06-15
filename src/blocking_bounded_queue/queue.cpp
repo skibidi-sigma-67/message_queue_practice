b9ed0d3b86ea00de8da54e15520e0305f427e938
Kirill Volychev <volychevk@gmail.com>
2026-06-15 17:07:20 +0300

    feat: implemented Size and GetStats functionality

--- src/blocking_bounded_queue/queue.cpp
+++ src/blocking_bounded_queue/queue.cpp
@@ -1,15 +1,20 @@
 #include <queue.hpp>
 
+#include <mutex>
+#include <condition_variable>
 #include <optional>
 
 size_t BlockingBoundedQueue::Size() const {
-    // TODO
-    return 0;
+    std::lock_guard<std::mutex> lock(mutex_);
+    return queue_.size();
 }
 
 QueueStats BlockingBoundedQueue::GetStats() const {
-    // TODO
-    return {};
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    QueueStats current_stats = stats_;
+    current_stats.current_size = queue_.size();
+    return current_stats;
 }
 
 PushStatus BlockingBoundedQueue::Push(Message message) {
@@ -28,5 +33,9 @@ std::optional<Message> BlockingBoundedQueue::WaitPop() {
 }
 
 void BlockingBoundedQueue::Close() {
-    // TODO
+    std::lock_guard<std::mutex> lock(mutex_);
+    is_closed_ = true;
+
+    cv_not_empty_.notify_all();
+    cv_not_full_.notify_all();
 }
